38d452bb3b8ee76381f2b596f271986d58de505a
salauddin <salauddin@bglobalsourcing.com>
2016-10-20 18:37:14 +0600

    stylesheet added as external source file and dashboard is designed with new post creation option and listing of previous post

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,5 +5,30 @@
 @endsection
 
 @section('content')
-<p>Could be trouble, could be trouble. hold the line. stay with me</p>
+<div class="row">
+    <div class="col-md-6">
+        <header><h3>What do you have to say?</h3></header>
+        <div class="form-group">
+            <textarea class="form-control" name="new-post" cols="30" rows="4"></textarea>
+        </div>
+        <input type="submit" class="btn btn-primary"/>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6">
+        <header><h3>What other people say...</h3></header>
+        <article>
+            <div>lorem ipsum</div>
+            <div>posted by on</div>
+            <div><a href="">like</a> <a href="">Dislike</a> <a href="">Edit</a></div>
+            <hr/>
+        </article>
+        <article>
+            <div>lorem ipsum</div>
+            <div>posted by on</div>
+            <div><a href="">like</a> <a href="">Dislike</a> <a href="">Edit</a></div>
+            <hr/>
+        </article>        
+    </div>
+</div>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{URL::to('css/style.css')}}"
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Styles -->
